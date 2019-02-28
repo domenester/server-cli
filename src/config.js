75796a12b6522c8702ab13382a72b7919b027f52
Diogo Domene <ddomene@interaxa.com>
2019-02-28 13:35:41 -0300

    Changing path in configs

--- src/config.js
+++ src/config.js
@@ -7,12 +7,12 @@ export const defaultEndpointValidation = 'endpoint-name.validation.txt';
 export const paths = {
   in: {
     api: {
-      source: () => (`${process.cwd()}/node_modules/server-cli/dist/structure/api`),
+      source: () => (`${process.cwd()}/node_modules/api-endpoint-cli/dist/structure/api`),
       root: function () { return `${this.source()}/api-name`},
       file: function () { return `${this.root()}/api-name.api.txt`}
     },
     endpoint: {
-      source: () => (`${process.cwd()}/node_modules/server-cli/dist/structure/endpoint`),
+      source: () => (`${process.cwd()}/node_modules/api-endpoint-cli/dist/structure/endpoint`),
       file: function () { return `${this.source()}/${defaultEndpointFile}`},
       spec: function () { return `${this.source()}/${defaultEndpointSpec}`},
       validation: function () { return `${this.source()}/${defaultEndpointValidation}`}
